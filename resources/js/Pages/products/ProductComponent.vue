6dcee75842e9d0d84b459a63da5d27eebcea26aa
Dang Xuan Danh <danhdx@deha-soft.com>
2025-01-01 21:58:26 +0700

    first commit

---
+++ resources/js/Pages/products/ProductComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="overflow-hidden overflow-x-auto p-6 bg-white border-b border-gray-200">
+                    <div class="min-w-full align-middle">
+                        <table class="min-w-full divide-y divide-gray-200 border">
+                            <thead>
+                                <tr>
+                                    <th class="px-6 py-3 bg-gray-50 text-left">
+                                        <span
+                                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</span>
+                                    </th>
+                                    <th class="px-6 py-3 bg-gray-50 text-left">
+                                        <span
+                                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                                <tr class="bg-white" v-for="product in products">
+                                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                        {{ product.name }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                        {{ product.content }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="mt-2">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const products = ref([]);
+
+
+onMounted(async () => {
+    await axios.get('/api/products')
+        .then(response => {
+            products.value = response.data.data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+})
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,11 @@ window.Alpine = Alpine;
 Alpine.start();
 import { createApp } from 'vue';
 import ComponentA from './components/ComponentA.vue';
+import ProductComponent from './Pages/products/ProductComponent.vue';
 
 const app = createApp({});
 
-app.component('ComponentA', ComponentA);
+app.component('ComponentA', ComponentA)
+    .component('ProductComponent', ProductComponent);
 
 app.mount("#app");
